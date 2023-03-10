145b68ee5e99edacb9f99ac6237fcbb7883f8ffe
Gilliady <gilliadyazevedo@gmail.com>
2023-03-10 14:15:10 -0300

    finalizado requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,24 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('handlerElephants is a function', () => {
+    expect(typeof handlerElephants).toBe('function');
+  });
+  it('if dont recive any param return undefined', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+  it('passed something different of a string as a param', () => {
+    expect(handlerElephants(1)).toEqual('Parâmetro inválido, é necessário uma string');
+  });
+  it('passed some independent information', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('uses computeData() correctyly', () => {
+    expect(handlerElephants('count')).toBe(4);
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+    expect(handlerElephants('averageAge')).toBe(10.5);
+    expect(handlerElephants('abublé')).toBeNull();
+  });
+});
