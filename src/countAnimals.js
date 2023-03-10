865df9dc9861e084916c774e10339c4eda15133e
Gilliady <gilliadyazevedo@gmail.com>
2023-03-10 13:31:43 -0300

    finalizado requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,21 @@ const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
   // seu código aqui
+  const objCounter = {};
+  switch (animal) {
+  case undefined:
+    data.species
+      .forEach((actualSpecie) => {
+        objCounter[actualSpecie.name] = actualSpecie.residents.length;
+      });
+    return objCounter;
+  default:
+    return !animal.sex ? data.species
+      .find((specie) => specie.name === animal.species).residents.length
+      : data.species
+        .find((specie) => specie.name === animal.species).residents
+        .filter((actualAnimal) => actualAnimal.sex === animal.sex).length;
+  }
 };
 
 module.exports = countAnimals;
