8af823539c38fd7b29dcad878eda7991f6a1f5a5
Gilliady <gilliadyazevedo@gmail.com>
2023-03-10 17:17:50 -0300

    finalizado requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
-const countEntrants = (entrants) => {
-  // seu código aqui
-};
+const countEntrants = (entrants) => ({
+  adult: entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50)
+    .length,
+  senior: entrants.filter((entrant) => entrant.age >= 50).length,
+  child: entrants.filter((entrant) => entrant.age < 18).length,
+});
 
 const calculateEntry = (entrants) => {
   // seu código aqui
+  let sum = 0;
+  if (entrants !== undefined) {
+    const countedEntrants = countEntrants(entrants);
+    Object.keys(countedEntrants).forEach((key) => {
+      sum += countedEntrants[key] * data.prices[key];
+    });
+  }
+  return sum;
 };
-
 module.exports = { calculateEntry, countEntrants };
